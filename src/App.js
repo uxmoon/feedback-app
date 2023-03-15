23b5b8985d83490fc2ce9731e7ce48789337d369
Martin Luna <uxmoon@gmail.com.com>
2023-03-15 17:38:26 -0300

    Import Header comp and pass props

--- src/App.js
+++ src/App.js
@@ -1,30 +1,12 @@
+import Header from './components/Header'
 function App() {
-  const title = 'Blog post'
-  const body = 'This is my blog post'
-  const comments = [
-    { id: 1, text: 'Comment one' },
-    { id: 2, text: 'Comment two' },
-    { id: 3, text: 'Comment three' },
-  ]
-  const loading = false
-  const showComments = true
-  if (loading) return <p>Loading...</p>
-  const commentsBlock = (
-    <div className='comments'>
-      <p>Comments ({comments.length})</p>
-      <ul>
-        {comments.map((comment, index) => (
-          <li key={index}>{comment.text}</li>
-        ))}
-      </ul>
-    </div>
-  )
   return (
-    <div className='container'>
-      <h1>{title}</h1>
-      <p>{body}</p>
-      {showComments && commentsBlock}
-    </div>
+    <>
+      <Header text='Feeback UI' />
+      <div className='container'>
+        <h1>My App</h1>
+      </div>
+    </>
   )
 }
 
