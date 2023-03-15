3fffe6c2bd9b94b2c8f056d07cef443dfa26e50d
Martin Luna <uxmoon@gmail.com.com>
2023-03-15 17:16:02 -0300

    Conditional content

--- src/App.js
+++ src/App.js
@@ -6,16 +6,24 @@ function App() {
     { id: 2, text: 'Comment two' },
     { id: 3, text: 'Comment three' },
   ]
-  return (
-    <div className='container'>
-      <h1>{title}</h1>
-      <p>{body}</p>
+  const loading = false
+  const showComments = true
+  if (loading) return <p>Loading...</p>
+  const commentsBlock = (
+    <div className='comments'>
       <p>Comments ({comments.length})</p>
       <ul>
         {comments.map((comment, index) => (
           <li key={index}>{comment.text}</li>
         ))}
       </ul>
+    </div>
+  )
+  return (
+    <div className='container'>
+      <h1>{title}</h1>
+      <p>{body}</p>
+      {showComments && commentsBlock}
     </div>
   )
 }
