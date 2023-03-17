ef1dbc58ceeac6d3eb48baf3d0f996805f31380f
Martin Luna <uxmoon@gmail.com.com>
2023-03-17 12:45:24 -0300

    Import and setup feedback stats component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import Header from './components/Header'
 import FeedbackList from './components/FeedbackList'
 import FeedbackData from './data/FeedbackData'
+import FeedbackStats from './components/FeedbackStats'
 function App() {
   const [feedback, setFeedback] = useState(FeedbackData)
   const deleteFeedback = (id) => {
@@ -13,6 +14,7 @@ function App() {
     <>
       <Header text='Feeback UI' />
       <div className='container'>
+        <FeedbackStats feedback={feedback} />
         <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
       </div>
     </>
