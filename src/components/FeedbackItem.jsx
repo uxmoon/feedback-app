17068e249063cd95580339be01dcdd51d9e3866e
Martin Luna <uxmoon@gmail.com.com>
2023-03-17 11:04:21 -0300

    Add default props and prop types

--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -1,11 +1,17 @@
+import PropTypes from 'prop-types'
 import Card from "./shared/Card"
+
 function FeedbackItem({item}) {
   return (
-    <Card reverse>
+    <Card>
       <div>{item.rating}</div>
       <p>{item.text}</p>
     </Card>
   )
+}
+
+FeedbackItem.propTypes = {
+  item: PropTypes.object.isRequired,
 }
 
 export default FeedbackItem
--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,4 +1,6 @@
+import PropTypes from 'prop-types'
 import FeedbackItem from './FeedbackItem'
+
 function FeedbackList({feedback}) {
   if(!feedback || feedback.length === 0) 
     return <p>No feedback yet.</p>
@@ -10,6 +12,16 @@ function FeedbackList({feedback}) {
           item={item} />
       ))}
     </div>
+  )
+}
+
+FeedbackList.propTypes = {
+  feedback: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      rating: PropTypes.number.isRequired,
+      text: PropTypes.string.isRequired
+    })
   )
 }
 
