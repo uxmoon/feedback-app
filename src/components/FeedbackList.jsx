d59973b085c51b417f14dda9b2921af0cb92ec90
Martin Luna <uxmoon@gmail.com.com>
2023-03-21 18:16:50 -0300

    Add animation options

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,3 +1,4 @@
+import { motion, AnimatePresence } from 'framer-motion'
 import PropTypes from 'prop-types'
 import FeedbackItem from './FeedbackItem'
 
@@ -5,11 +6,31 @@ function FeedbackList({ feedback, handleDelete }) {
   if (!feedback || feedback.length === 0) return <p>No feedback yet.</p>
   return (
     <div>
-      {feedback.map((item) => (
-        <FeedbackItem key={item.id} item={item} handleDelete={handleDelete} />
-      ))}
+      <AnimatePresence>
+        {feedback.map((item) => (
+          <motion.div
+            key={item.id}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+          >
+            <FeedbackItem
+              key={item.id}
+              item={item}
+              handleDelete={handleDelete}
+            />
+          </motion.div>
+        ))}
+      </AnimatePresence>
     </div>
   )
+  // return (
+  //   <div>
+  //     {feedback.map((item) => (
+  //       <FeedbackItem key={item.id} item={item} handleDelete={handleDelete} />
+  //     ))}
+  //   </div>
+  // )
 }
 
 FeedbackList.propTypes = {
