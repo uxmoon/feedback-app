af96e63478ceff59ccff04ac2f65c5c48ee074ca
Martin Luna <uxmoon@gmail.com.com>
2023-03-21 17:19:13 -0300

    Add button component

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import Card from './shared/Card'
+import Button from './shared/Button'
 function FeedbackForm() {
   const [text, setText] = useState('')
   const handleTextChange = (event) => {
@@ -17,7 +18,7 @@ function FeedbackForm() {
             onChange={handleTextChange}
             value={text}
           />
-          <button type='submit'>Send</button>
+          <Button type='submit'>Send</Button>
         </div>
       </form>
     </Card>
