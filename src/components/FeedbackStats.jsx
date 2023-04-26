bdecb74c2a9f9ef76b01c06b4921c17cab02fa4f
uxmoon <uxmoon@gmail.com>
2023-04-26 11:02:50 -0300

    Add comment

--- src/components/FeedbackStats.jsx
+++ src/components/FeedbackStats.jsx
@@ -7,6 +7,7 @@ function FeedbackStats({ feedback }) {
       return acc + cur.rating
     }, 0) / feedback.length
 
+  // Display only one decimal and remove zero
   average = average.toFixed(1).replace(/[.,]0$/, '')
   return (
     <div className='feedback-stats'>
