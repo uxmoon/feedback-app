f57aab8c9ccf5017bd8f488c4c759d53dd4c0817
uxmoon <uxmoon@gmail.com>
2023-08-28 16:45:39 -0300

    Use provider data in child component

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,25 +1,21 @@
-import PropTypes from 'prop-types'
-import FeedbackItem from './FeedbackItem'
+import { useContext } from 'react';
+import FeedbackContext from '../context/FeedbackContext';
 
-function FeedbackList({ feedback, handleDelete }) {
-  if (!feedback || feedback.length === 0) return <p>No feedback yet.</p>
+import FeedbackItem from './FeedbackItem';
+
+function FeedbackList({ handleDelete }) {
+  const { feedback } = useContext(FeedbackContext);
+  console.log(feedback);
+
+  if (!feedback || feedback.length === 0) return <p>No feedback yet.</p>;
+
   return (
     <div className='feedback-list'>
       {feedback.map((item) => (
         <FeedbackItem key={item.id} item={item} handleDelete={handleDelete} />
       ))}
     </div>
-  )
+  );
 }
 
-FeedbackList.propTypes = {
-  feedback: PropTypes.arrayOf(
-    PropTypes.shape({
-      id: PropTypes.number.isRequired,
-      rating: PropTypes.number.isRequired,
-      text: PropTypes.string.isRequired,
-    })
-  ),
-}
-
-export default FeedbackList
+export default FeedbackList;
