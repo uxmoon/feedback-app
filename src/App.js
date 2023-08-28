85d0ec9e109b99b5a307b15b1f3b5bf586b9f83e
uxmoon <uxmoon@gmail.com>
2023-08-28 15:49:04 -0300

    Add context

--- src/App.js
+++ src/App.js
@@ -1,31 +1,33 @@
-import { v4 as uuidv4 } from 'uuid'
-import { useState } from 'react'
-import Header from './components/Header'
-import FeedbackList from './components/FeedbackList'
-import FeedbackData from './data/FeedbackData'
-import FeedbackStats from './components/FeedbackStats'
-import FeedbackForm from './components/FeedbackForm'
+import { v4 as uuidv4 } from 'uuid';
+import { useState } from 'react';
+import Header from './components/Header';
+import FeedbackList from './components/FeedbackList';
+import FeedbackData from './data/FeedbackData';
+import FeedbackStats from './components/FeedbackStats';
+import FeedbackForm from './components/FeedbackForm';
+import { FeedbackProvider } from './context/FeedbackContext';
+
 function App() {
-  const [feedback, setFeedback] = useState(FeedbackData)
+  const [feedback, setFeedback] = useState(FeedbackData);
   const addFeedback = (newFeedback) => {
-    newFeedback.id = uuidv4()
-    setFeedback([newFeedback, ...feedback])
-  }
+    newFeedback.id = uuidv4();
+    setFeedback([newFeedback, ...feedback]);
+  };
   const deleteFeedback = (id) => {
     if (window.confirm('Are you sure you want to delete?')) {
-      setFeedback(feedback.filter((item) => item.id !== id))
+      setFeedback(feedback.filter((item) => item.id !== id));
     }
-  }
+  };
   return (
-    <>
+    <FeedbackProvider>
       <Header text='Feedback UI' />
       <div className='container'>
         <FeedbackForm handleAdd={addFeedback} />
         <FeedbackStats feedback={feedback} />
         <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
       </div>
-    </>
-  )
+    </FeedbackProvider>
+  );
 }
 
-export default App
+export default App;
