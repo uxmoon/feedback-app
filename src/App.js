abf495155bc3c1f9a08e066ef926fa7db14923b8
uxmoon <uxmoon@gmail.com>
2023-08-29 11:30:45 -0300

    Add routes for main and about pages

--- src/App.js
+++ src/App.js
@@ -1,20 +1,37 @@
-import Header from './components/Header';
-import FeedbackList from './components/FeedbackList';
-import FeedbackStats from './components/FeedbackStats';
-import FeedbackForm from './components/FeedbackForm';
-import { FeedbackProvider } from './context/FeedbackContext';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Header from './components/Header'
+import FeedbackList from './components/FeedbackList'
+import FeedbackStats from './components/FeedbackStats'
+import FeedbackForm from './components/FeedbackForm'
+import { FeedbackProvider } from './context/FeedbackContext'
+import AboutIconLink from './components/AboutIconLink'
+import AboutPage from './pages/AboutPage'
 
 function App() {
   return (
     <FeedbackProvider>
-      <Header text='Feedback UI' />
-      <div className='container'>
-        <FeedbackForm />
-        <FeedbackStats />
-        <FeedbackList />
-      </div>
+      <Router>
+        <Header text='Feedback UI' />
+        <div className='container'>
+          <Routes>
+            <Route
+              path='/'
+              exact
+              element={
+                <>
+                  <FeedbackForm />
+                  <FeedbackStats />
+                  <FeedbackList />
+                </>
+              }
+            ></Route>
+            <Route path='/about' element={<AboutPage />} />
+          </Routes>
+        </div>
+        <AboutIconLink />
+      </Router>
     </FeedbackProvider>
-  );
+  )
 }
 
-export default App;
+export default App
